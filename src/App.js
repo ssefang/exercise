b2e7e3033da952e2f8f5506f921594a14b72517f
ssefang <91700513+ssefang@users.noreply.github.com>
2022-06-22 09:52:06 -0400

    a

--- src/App.js
+++ src/App.js
@@ -48,19 +48,19 @@ function App() {
             <Route path="/" element={<MainLayout />}>
               <Route index element={<Home />} />
               <Route path="about" element={<About />} />
-              <Route path="login" element={<LoginForm />} />
+              {/* <Route path="login" element={<LoginForm />} />
               <Route path="sign-up" element={<RegisterForm />} />
-              <Route path="signout" element={<SignOut />} />
+              <Route path="signout" element={<SignOut />} /> */}
               <Route path="kids" element={<Kids />} />
               <Route path="*" element={<Navigate to="/" />} />
             </Route>
-            {/* <Route element={<DashboardLayout />}>
+            <Route element={<DashboardLayout />}>
               <Route element={<SidebarLayout />}>
                 <Route path="/post" element={<Post />} />
                 <Route path="/itemList" element={<ItemList />} />
                 <Route path="/edit/:id" element={<Update />} />
               </Route>
-            </Route> */}
+            </Route>
           </Routes>
         </UserInContext.Provider>
       </LoggedInContext.Provider>
